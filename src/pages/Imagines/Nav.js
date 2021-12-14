2ee50e6b4d4778862d5cc7618f3b55da00c4c5ba
aindrila22 <aindrilabhattacharjee72@gmail.com>
2021-12-14 19:23:14 +0530

    update routes

--- src/pages/Imagines/Nav.js
+++ src/pages/Imagines/Nav.js
@@ -11,7 +11,10 @@ import Search from "../../components/Search";
 const Nav = () => {
   return (
     <div className="flex justify-around items-center font-Libre text-gray-800 pt-2 pb-3">
-      <ArrowLeftIcon className="h-5 w-5" />
+      <Link to="/">
+        {" "}
+        <ArrowLeftIcon className="h-5 w-5" />
+      </Link>
       <Link
         to="/"
         className="flex items-center py-1.5 px-2 pl-1 gap-1  ml-5 mr-8 rounded-3xl transition duration-200 bg-gray-200 text-gray-800 focus:bg-cyan-900 dark:hover:bg-cyan-900 hover:bg-teal-800 hover:text-gray-100"
