b7d95acfd0a8266bcdfa1c99826b26af649a8a71
Sayan Mondal <sayanm816@gmail.com>
2021-12-30 13:32:19 +0530

    new update

--- src/pages/Blog/Card.js
+++ src/pages/Blog/Card.js
@@ -43,7 +43,6 @@ const Card = ({
   };
   return (
     <>
-
       {/* <motion.div
         initial={{ opacity: 0.5 }}
         animate={{ opacity: 1 }}
@@ -94,47 +93,28 @@ const Card = ({
               <span className="text-xs text-gray-400">12-12-21</span>
             </span>
           </div>
-          <div className="flex justify-between items-center bg-gray-100 shadow-md rounded-b-lg">
-            <div className="flex items-center space-x-1 px-3 py-2">
-              {/* <img src={avatar} alt="dp" className="w-8 h-8 rounded-full" /> */}
-              <img
-                src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg"
-                alt="dp"
-                className="w-8 h-8 rounded-full"
-              />
-              <span className="flex flex-col font-medium items-start px-2">
-                <span className="text-sm text-black font-semibold">{user}</span>
-                <span className="text-xs text-gray-400">12-12-21</span>
+          <div className="flex items-start space-x-5 pr-3">
+            <span className="flex items-center text-xs">
+              <span className="cursor-pointer" onClick={clickLikeHandler}>
+                {like ? (
+                  <LightBulbIcon className="h-7 w-7 text-yellow-400" />
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-7 w-7 text-yellow-600"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+                    />
+                  </svg>
+                )}
               </span>
-            </div>
-            <div className="flex items-start space-x-5 pr-3">
-              <span className="flex items-center text-xs">
-                <span className="cursor-pointer" onClick={clickLikeHandler}>
-                  {like ? (
-                    <LightBulbIcon className="h-7 w-7 text-yellow-400" />
-                  ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-7 w-7 text-yellow-600"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-                      />
-                    </svg>
-                  )}
-                </span>
-                <i>{12}</i>
-              </span>
-              <span>
-                <Sound />
-              </span>
-
               <i>{likes ? likes.length : 0}</i>
             </span>
             <span>
@@ -160,9 +140,8 @@ const Card = ({
                     />
                   </svg>
                 )}
-
               </span>
-            </div>
+            </span>
           </div>
         </div>
       </div>
