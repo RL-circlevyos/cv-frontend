fd4f71063121e6f4ee99806b14c9383c57873170
aindrila22 <aindrilabhattacharjee72@gmail.com>
2022-02-01 00:27:26 +0530

    create imagine publish button update

--- src/pages/Imagines/General-Imagines/Create/Header.js
+++ src/pages/Imagines/General-Imagines/Create/Header.js
@@ -2,33 +2,63 @@ import { ArrowLeftIcon } from "@heroicons/react/solid";
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Header = () => {
+const Header = ({ title }) => {
   return (
-    <div className=" w-full font-Mulish text-sm text-gray-800 pt-1 pb-1">
-      <div className="lg:flex items-center space-x-2 hidden w-full justify-between px-3 py-2">
-        <div className="flex items-center space-x-5 ">
-          <Link to="/">
-            <ArrowLeftIcon className="h-5 w-5 mr-4" />{" "}
-          </Link>
-        </div>
-        <div className="text-2xl font-medium  text-primary">
-          Create Imagines
-        </div>
-        <div></div>
-        {/* <Link
+    <div className="w-full grid place-items-center font-Mulish text-sm text-gray-800 pb-1">
+      <div className="lg:block hidden fixed w-3/5 bg-gray-50 shadow z-50 pb-3 mt-12 px-4 py-2">
+        <div className="flex justify-between items-center w-full max-w-6xl">
+          <div className="flex items-center space-x-5 ">
+            <Link to="/">
+              <ArrowLeftIcon className="h-5 w-5 mr-4" />{" "}
+            </Link>
+          </div>
+          <div className="text-2xl font-medium  text-primary">
+            Create Imagines
+          </div>
+          <div className="flex items-center space-x-2 mt-4">
+            {!title ? (
+              <button className="py-1.5 lg:py-2 lg:px-3 px-2 font-bold rounded-sm text-sm lg:text-base transition duration-200 bg-gray-300 text-white ">
+                Publish
+              </button>
+            ) : (
+              <button
+                type="submit"
+                className="py-1.5 lg:py-2 lg:px-3 px-2 font-bold rounded-sm text-sm 
+                lg:text-base transition duration-200 bg-primary text-gray-50 focus:bg-cyan-900 dark:hover:bg-cyan-900 hover:bg-teal-800 hover:text-gray-100"
+              >
+                Publish
+              </button>
+            )}
+          </div>
+          {/* <Link
           to="/"
           className="flex font-bold items-center py-1.5 px-2  rounded-3xl transition duration-200 bg-gray-200 text-gray-800 focus:bg-cyan-900 dark:hover:bg-cyan-900 hover:bg-teal-800 hover:text-gray-100"
         >
           Saved Imagines
         </Link> */}
+        </div>
       </div>
-      <div className="block lg:hidden">
-        <div className="text-lg font-bold uppercase text-primary flex justify-center items-start px-3 mt-5">
+      <div className="block lg:hidden fixed w-full bg-gray-50 shadow z-50 pb-3 mt-14">
+        <div className="text-lg font-bold uppercase text-primary flex justify-between items-start px-3 mt-4 ">
           <Link to="/">
             <ArrowLeftIcon className="h-5 w-5 mr-4" />{" "}
           </Link>
           <span>Create Imagines</span>
-          <span></span>
+          <div className="flex items-center space-x-2 ">
+            {!title ? (
+              <button className="py-1.5 lg:py-2 lg:px-3 px-2 font-bold rounded-sm text-sm lg:text-base transition duration-200 bg-gray-300 text-white ">
+                Publish
+              </button>
+            ) : (
+              <button
+                type="submit"
+                className="py-1.5 lg:py-2 lg:px-3 px-2 font-bold rounded-sm text-sm 
+                lg:text-base transition duration-200 bg-primary text-gray-50 focus:bg-cyan-900 dark:hover:bg-cyan-900 hover:bg-teal-800 hover:text-gray-100"
+              >
+                Publish
+              </button>
+            )}
+          </div>
         </div>
       </div>
     </div>
