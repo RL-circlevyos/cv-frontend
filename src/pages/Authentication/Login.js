ff8e73894f02b444ad054b5e20fe1aa03ef186bd
sayanmondal31 <sayanm816@gmail.com>
2022-03-03 19:39:20 +0530

    google login omit

--- src/pages/Authentication/Login.js
+++ src/pages/Authentication/Login.js
@@ -201,13 +201,13 @@ const Login = () => {
                 </span>
               </Link>
 
-              <GoogleLogin
+              {/* <GoogleLogin
                 clientId="1047426319195-6dndogses33r7jku9k87gkhf9esmagee.apps.googleusercontent.com"
                 buttonText="Login With google"
                 onSuccess={responseGoogle}
                 // onFailure={responseGoogle}
                 cookiePolicy={"single_host_origin"}
-              />
+              /> */}
             </div>
           </div>
         </div>
